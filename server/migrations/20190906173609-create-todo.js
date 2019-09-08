65fac797c957ee57c7fc6bd284bb604c26f635ed
Dharti Madeka <dpmadeka@dons.usfca.edu>
2019-09-08 13:58:25 -0700

    Todo App using nodeJS, postgress, express and sequelize

---
+++ server/migrations/20190906173609-create-todo.js
@@ -0,0 +1,24 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => //to write command related to table
+    queryInterface.createTable('Todos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    }),
+  down: (queryInterface /* , Sequelize */) => queryInterface.dropTable('Todos'), // to write reverse process of wat is in up
+};
